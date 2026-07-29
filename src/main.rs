d5e5aaa30f295e9b695da11554f97991c658d387
Käyttäjä <radymav@proton.me>
2026-07-29 13:06:30 +0300

    Third commit

--- src/main.rs
+++ src/main.rs
@@ -12,34 +12,50 @@ fn multiply(a: i64, b: i64) -> i64 {
     a * b
 }
 
-fn divide(a: i64, b: i64) -> i64 {
-    a / b
+fn divide(a: i64, b: i64) -> f64 {
+    a as f64 / b as f64
 }
 
 
 
 fn main() {
 
-    println!("Enter first number: ");
-    let mut a: String = String::new();
-    io::stdin().read_line(&mut a).expect("Failde to read lline");
-    let a: i64 = a.trim().parse().expect("Enter a number!");
+    loop {
 
-    println!("Enter second number: ");
-    let mut b: String = String::new();
-    io::stdin().read_line(&mut b).expect("Failed to read line");
-    let b: i64 = b.trim().parse().expect("Enter a number!");
+        println!("Enter first number: ");
+        let mut a: String = String::new();
+        io::stdin().read_line(&mut a).expect("Failde to read lline");
+        let a: i64 = a.trim().parse().expect("Enter a number!");
 
-
-    let result_a: i64 = addition(a, b);
-    let result_s: i64 = subtraction(a, b);
-    let result_m: i64 = multiply(a, b);
-    let result_d: i64 = divide(a, b);
-
-    println!("{} + {} = {}", a, b, result_a);
-    println!("{} - {} = {}", a, b, result_s);
-    println!("{} * {} = {}", a, b, result_m);
-    println!("{} / {} = {}", a, b, result_d);
+        println!("Enter second number: ");
+        let mut b: String = String::new();
+        io::stdin().read_line(&mut b).expect("Failed to read line");
+        let b: i64 = b.trim().parse().expect("Enter a number!");
 
 
+        println!("+ is addition");
+        println!("- is subtraction");
+        println!("* is multiply");
+        println!("/ is divide");
+        println!("' is exit");
+
+        println!("Enter the operation: ");
+        let mut op: String = String::new();
+        io::stdin().read_line(&mut op).expect("Failed to read line");
+        let op = op.trim();
+
+
+
+        match op {
+            "+" => println!("{} + {} = {}", a, b, addition(a, b)),
+            "-" => println!("{} - {} = {}", a, b, subtraction(a, b)),
+            "*" => println!("{} * {} = {}", a, b, multiply(a, b)),
+            "/" => println!("{} / {} = {}", a, b, divide(a, b)),
+            _ => println!("Result: No operation"),
+        }
+
+        if op == "'" {
+            break
+        }
+    }
 }
